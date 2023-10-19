081d743888d5eeaafbf2292b38a78502496ee5a2
soar1219 <tsubasa1219.info@gmail.com>
2023-10-19 16:17:04 +0900

    コンテンツの表示

--- src/pages/blog/[ID].tsx
+++ src/pages/blog/[ID].tsx
@@ -1,5 +1,11 @@
 import { client } from "@/libs/client";
 import { Blog } from "@/types/blog";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+dayjs.extend(utc);
+dayjs.extend(timezone);
+import styles from "@/styles/Blogid.module.css";
 
 type Props = {
   blog: Blog[];
@@ -8,13 +14,26 @@ type Props = {
 const BlogId: React.FC<Props> = ({ blog }: any) => {
   return (
     <main>
-      <h1>{blog.title}</h1>
-      <p>{blog.publishedAt}</p>
-      <div
-        dangerouslySetInnerHTML={{
-          __html: `${blog.content}`,
-        }}
-      />
+      <div>
+        <h1 className={styles.title}>{blog.title}</h1>
+        <p className={styles.publishedAt}>
+          投稿日：
+          {dayjs
+            .utc(blog.publishedAt)
+            .tz("Asia/Tokyo")
+            .format(
+              "YYYY" + "年" + "MM" + "月" + "DD" + "日" + "hh" + ":" + "mm"
+            )}
+        </p>
+        <p className={styles.category}>
+          タグ：{blog.category && `${blog.category.name}`}
+        </p>
+
+        <div
+          className={styles.post}
+          dangerouslySetInnerHTML={{ __html: blog.content }}
+        />
+      </div>{" "}
     </main>
   );
 };
@@ -22,7 +41,7 @@ const BlogId: React.FC<Props> = ({ blog }: any) => {
 export default BlogId;
 
 export const getStaticPaths = async () => {
-  const data = await client.get({ endpoint: "blog" });
+  const data = await client.get({ endpoint: "blogs" });
 
   const paths = data.contents.map(
     (content: { id: string }) => `/blog/${content.id}`
--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -1,33 +1,70 @@
 import Link from "next/link";
 import { client } from "@/libs/client";
 import { Blog } from "@/types/blog";
+import dayjs from "dayjs";
+import utc from "dayjs/plugin/utc";
+import timezone from "dayjs/plugin/timezone";
+dayjs.extend(utc);
+dayjs.extend(timezone);
+
+import styles from "@/styles/Post.module.css";
+import Image from "next/image";
 
 type Props = {
   blog: Blog[];
 };
 
 const Posts: React.FC<Props> = ({ blog }) => {
-  return (
-    <div>
-      <ul>
-        {blog.map((blog) => (
-          <li key={blog.id}>
-            <Link href={`/blog/${blog.id}`}>{blog.title}</Link>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-};
-
-export default Posts;
-
-export const getStaticProps = async () => {
-  const data = await client.get({ endpoint: "blog" });
-
-  return {
-    props: {
-      blog: data.contents,
-    },
+    return (
+      <div>
+        <div className={styles.posts}>
+          <ul className={styles.grid}>
+            {blog.map((blog) => (
+              <li key={blog.id}>
+                <Link href={`/blog/${blog.id}`}>
+                  <Image
+                    className={styles.eyecatch_img}
+                    src={blog.eyecatch.url}
+                    width={300}
+                    height={175}
+                    alt={blog.title}
+                  />
+                  <h5 className={styles.blogtitle}>{blog.title}</h5>
+                  <p className={styles.publishedAt}>
+                    投稿日：
+                    {dayjs
+                      .utc(blog.publishedAt)
+                      .tz("Asia/Tokyo")
+                      .format(
+                        "YYYY" +
+                          "年" +
+                          "MM" +
+                          "月" +
+                          "DD" +
+                          "日" +
+                          "hh" +
+                          ":" +
+                          "mm"
+                      )}
+                  </p>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </div>
+    );
   };
-};
+  
+  export default Posts;
+  
+  // データをテンプレートに受け渡す部分の処理を記述します
+  export const getStaticProps = async () => {
+    const data = await client.get({ endpoint: "blogs" });
+  
+    return {
+      props: {
+        blog: data.contents,
+      },
+    };
+  };
